43116d2d1dce2e026558d2c4148cf3046c092174
imaginaryverse <bertilsson.anton@gmail.com>
2021-06-26 20:50:22 +0200

    minor fixes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Todo = ({ todo, toggleDone, removeTodo }) => {
   return (
-    <li className={todo.done ? 'todo done' : 'todo'}>
+    <li
+      className={todo.done ? 'todo done' : 'todo'}
+      onClick={() => toggleDone(todo.id)}
+    >
       <div className='todo__info-container'>
         <p className='todo__info-container__date'>{todo.date}</p>
         <h3 className='todo__info-container__task'>{todo.task}</h3>
         <p className='todo__info-container__description'>{todo.description}</p>
       </div>
       <div className='todo__btn-container'>
-        <button
+        {/* <button
           type='button'
           className='done-btn'
           onClick={() => toggleDone(todo.id)}
         >
           ✓
-        </button>
-        <button
+        </button> */}
+        {/* <button
           type='button'
           className='remove-btn'
           onClick={() => removeTodo(todo.id)}
         >
           ✕
+        </button> */}
+        <button
+          type='button'
+          className='remove-btn'
+          onClick={() => removeTodo(todo.id)}
+        >
+          Remove
         </button>
+        {/* <button className='edit-btn'>Edit</button> */}
       </div>
     </li>
   );
